e5ef8fa0160df07f433a405887777afdf9a79e43
gilberd30 <gilberd30@gmail.com>
2021-06-11 20:07:07 +0700

    first commit (pure code dari pertemuan 9)

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HelloWorld  />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+
+}
+</style>
+
+{
+  nama : 'budi'
+  umur: 20,
+  minum: function() {
+    this.haus
+  }
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+ <div>
+  <h1> Selamat datang </h1>
+  <ul>
+    <li v-for="item in todos">{{ item.desc}}</li>
+  </ul>
+  <input v-model="myText"/>
+  <button @click="tambah">Add</button>
+ </div>
+</template>
+
+<script>
+export default { 
+data: function () {
+  return {
+    todos: [
+      { desc: 'Makan Pisang'},
+      { desc: 'Makan Sayur'},
+      { desc: 'Belajar JS'},
+      { desc: 'Pulang'},
+      { desc: 'Mangstaps'}
+      
+    ],
+    myText: ''
+  }
+},
+methods: {
+  tambah: function () {
+    this.todos.push({desc: this.myText})
+  }
+}
+}
+</script>
+
+
+
+
+
+
