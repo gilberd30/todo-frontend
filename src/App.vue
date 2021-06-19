92ac47070f0de60fe01cf3b14cb7bd6705ea988f
gilberd30 <gilberd30@gmail.com>
2021-06-19 07:43:49 +0700

    menambahkan comoponents user.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <todo  />
+  <user  />
 </template>
 
 <script setup>
 import todo from './components/todo.vue'
+import user from './components/user.vue'
 </script>
---
+++ src/components/user.vue
@@ -0,0 +1,52 @@
+<template>
+ <div>
+  <h1> Berikut adalah pengguna kita </h1>
+  <ul>
+    <li v-for="item in users" :key="item.id">{{ item.deskripsi}}
+      <button @click="hapus(item.id)">&times;</button>
+    </li>
+  </ul>
+  <input v-model="username"/>
+  <input v-model="password"/>
+  <button @click="tambah">Add</button>
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default { 
+data: function () {
+  return {
+    users: [],
+    username: '',
+    password: ''
+  }
+},
+
+created: function () {
+  axios.get('http://localhost:3031/user')
+  .then(result=>{
+    this.users = result.data
+  })
+},
+
+methods: {
+  tambah: function () {
+    const newItem = { deskripsi: this.myText}
+    axios.post('http://localhost:3031/user', newItem)
+    this.users.push(newItem)
+  },
+  hapus: function (id) {
+    const index = this.users.indexOf(id)
+    axios.delete(`http://localhost:3031/user/${id}`)
+    this.users.splice(index,1)
+  }
+}
+}
+</script>
+
+
+
+
+
+
