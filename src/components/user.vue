a1a5b0772fa661d8bb37ee43db0d107d539d3ff7
gilberd30 <gilberd30@gmail.com>
2021-06-19 09:04:57 +0700

    menambahkan konfigurasi auth pada component user

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,50 +1,50 @@
 <template>
- <div>
-  <h1> Berikut adalah pengguna kita </h1>
-  <ul>
-    <li v-for="item in users" :key="item.id">{{ item.deskripsi}}
-      <button @click="hapus(item.id)">&times;</button>
-    </li>
-  </ul>
-  <input v-model="username"/>
-  <input v-model="password"/>
-  <button @click="tambah">Add</button>
- </div>
+  <div>
+    <h1>Berikut adalah pengguna kita</h1>
+    <ul>
+      <li v-for="item in users" :key="item.id">
+        {{ item.user }}
+        <button @click="hapus(item.id)">&times;</button>
+      </li>
+    </ul>
+    <input v-model="username" />
+    <input v-model="password" />
+    <button @click="tambah">Add</button>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-export default { 
-data: function () {
-  return {
-    users: [],
-    username: '',
-    password: ''
-  }
-},
-
-created: function () {
-     this.username = localStorage.getItem("usr");
-    this.password = localStorage.getItem("pwd");
-  axios.get('http://localhost:3031/user',{ headers: { username, password } })
-  .then(result=>{
-    this.users = result.data
-  })
-},
-
-methods: {
-  tambah: function () {
-    const newItem = { deskripsi: this.myText}
-    axios.post('http://localhost:3031/user', newItem)
-    this.users.push(newItem)
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      users: [],
+      username: "",
+      password: "",
+    };
   },
-  hapus: function (id) {
-    const index = this.users.indexOf(id)
-    axios.delete(`http://localhost:3031/user/${id}`)
-    this.users.splice(index,1)
-  }
-}
-}
+
+  created: function () {
+    const username = localStorage.getItem("usr");
+    const password = localStorage.getItem("pwd");
+    axios.get("http://localhost:3031/user", { headers: { username, password } }).then((result) => {
+      this.users = result.data;
+    });
+  },
+
+  methods: {
+    tambah: function () {
+      const username = localStorage.getItem("usr");
+      const password = localStorage.getItem("pwd");
+      axios.post("http://localhost:3031/user", {
+        headers: { username, password },
+      });
+    },
+    hapus: function (id) {
+      axios.delete(`http://localhost:3031/user/${id}`);
+    },
+  },
+};
 </script>
 
 
